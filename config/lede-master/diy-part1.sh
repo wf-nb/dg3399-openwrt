5bb1ca65af79917e05ea44c44400e2a47106eced
Lemon <149338935+Lemon1151@users.noreply.github.com>
2024-06-16 19:35:13 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -7,7 +7,10 @@
 #========================================================================================================================
 
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a\src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default  
+# sed -i '$a\src-git istore https://github.com/linkease/istore;main' feeds.conf.default  
+# sed -i '$a\src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default  
+# sed -i '$a\src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
